a54d046bbff94885b4431125a834c5a7124dc55f
JackHenry-G <Jack_HG@outlook.com>
2024-02-28 17:08:54 +0000

    Added ability for user to search by postcode

--- src/main/java/com/goggin/restaurantdata/Controller/RestaurantController.java
+++ src/main/java/com/goggin/restaurantdata/Controller/RestaurantController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.goggin.restaurantdata.Model.Restaurant;
 import com.goggin.restaurantdata.Service.RestaurantService;
@@ -18,14 +19,9 @@ public class RestaurantController {
     @Autowired
     private RestaurantService restaurantService;
 
-    @GetMapping
-    public String getRestaurants(Model model) {
-        String test_postcode = "EC4M7RF";
-
-        List<Restaurant> restaurants = restaurantService.findRestaurantsByPostcode(test_postcode);
-        for (Restaurant restaurant : restaurants) {
-            System.out.println("Restaurant: " + restaurant);
-        }
+    @GetMapping // e.g. http://localhost:8080/restaurants?postcode=EC4M7RF
+    public String getRestaurantsByPostcode(@RequestParam(name = "postcode") String postcode, Model model) {
+        List<Restaurant> restaurants = restaurantService.findRestaurantsByPostcode(postcode);
 
         model.addAttribute("restaurants", restaurants);
         return "restaurant";
